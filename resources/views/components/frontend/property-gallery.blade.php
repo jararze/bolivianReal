8a87cdba8f7a3ced3c461350f14cd411d01a9e82
Jorge <jararze@gmail.com>
2025-02-25 22:42:13 -0400

    ultimos cambios

--- resources/views/components/frontend/property-gallery.blade.php
+++ resources/views/components/frontend/property-gallery.blade.php
@@ -1,13 +1,13 @@
 {{-- resources/views/components/property-gallery.blade.php --}}
 <div class="single-property-slider gallery-slider flexslider">
     <ul class="slides">
-{{--        @dd($images)--}}
         @foreach($images as $image)
+{{--            @dd($image->name)--}}
             <li>
                 <a class="swipebox"
                    data-rel="gallery"
-                   href="{{ $image->image_url }}">
-                    <img src="{{ asset('storage/' . $image->image_url) }}"
+                   href="{{ asset('storage/' . $image->name) }}">
+                    <img src="{{ asset('storage/' . $image->name) }}"
                          style="height: 510px; width: 100%; object-fit: cover;"
                          alt="{{ $name }}">
                 </a>
